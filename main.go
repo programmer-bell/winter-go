5657c1192d31ce197f52b6a8ec239cd958f392b7
Badhan Sarkar <badhansarkar164@gmail.com>
2025-11-29 23:29:11 +0530

    writing our project logic

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	// fmt.Print("Hello World\n")
-	fmt.Println("Hello World")
-	fmt.Print("Understand golang")
+	// fmt.Println("Hello World")
+	// fmt.Print("Understand golang")
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
 }
