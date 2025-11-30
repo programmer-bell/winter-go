211d10b8dc9b42a5893f7c53cac0cadf917d701e
Badhan Sarkar <badhansarkar164@gmail.com>
2025-11-30 09:41:02 +0530

    formatting text in print

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ func main() {
 	// fmt.Println("Hello World")
 	// fmt.Print("Understand golang")
 
-	fmt.Println("Welcome to our ",conferenceName ,"booking application")
-	fmt.Println("We have total of",conferenceTickets, "tickets and",reminingTickets,"tickets are available")
+	// fmt.Println("Welcome to our ",conferenceName ,"booking application")
+
+	fmt.Printf("Welcome to our %v booking application\n",conferenceName)
+
+	// fmt.Println("We have total of",conferenceTickets, "tickets and",reminingTickets,"tickets are available")
+
+	fmt.Printf("We have total of %v tickets and %v tickets are available\n",conferenceTickets,reminingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	// var name = "Go Confirence"
