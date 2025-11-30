331fc639c844581e497b2939026c3d55042cb33f
Badhan Sarkar <badhansarkar164@gmail.com>
2025-11-30 09:38:00 +0530

    Understanding variables & their usecase

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import "fmt"
 
 func main() {
+
+	var conferenceName = "Go Confirence"
+	const conferenceTickets = 50
+	var reminingTickets = 50
+
 	// fmt.Print("Hello World\n")
 	// fmt.Println("Hello World")
 	// fmt.Print("Understand golang")
-	fmt.Println("Welcome to our conference booking application")
+
+	fmt.Println("Welcome to our ",conferenceName ,"booking application")
+	fmt.Println("We have total of",conferenceTickets, "tickets and",reminingTickets,"tickets are available")
 	fmt.Println("Get your tickets here to attend")
-}
+
+	// var name = "Go Confirence"
+	// fmt.Print(name)
+
+	}
