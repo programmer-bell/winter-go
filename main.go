48d19a9ac05b243e37959f129ae1c28b6a9dd144
Badhan Sarkar <badhansarkar164@gmail.com>
2025-11-30 21:39:26 +0530

    understanding loops in go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
@@ -50,42 +53,52 @@ func main() {
 	// fmt.Scanf("%s %d", &name, &age)
 	// fmt.Println("The username is:",name,"and age is:", age)
 
-	var fname string
-	var lname string
-	var age uint
-	var email string
-	var userTickets uint
+	for{
 
-	fmt.Print("Enter your first name:")
-	fmt.Scanln(&fname)
-	fmt.Print("Enter your last name:")
-	fmt.Scanln(&lname)
-	fmt.Print("Enter your age:")
-	fmt.Scanln(&age)
-	fmt.Print("Enter your email address:")
-	fmt.Scanln(&email)
-	fmt.Print("Enter your booking tickets:")
-	fmt.Scanln(&userTickets)
+			var fname string
+			var lname string
+			var age uint
+			var email string
+			var userTickets uint
 
-	reminingTickets -=  userTickets
+			fmt.Print("Enter your first name:")
+			fmt.Scanln(&fname)
+			fmt.Print("Enter your last name:")
+			fmt.Scanln(&lname)
+			fmt.Print("Enter your age:")
+			fmt.Scanln(&age)
+			fmt.Print("Enter your email address:")
+			fmt.Scanln(&email)
+			fmt.Print("Enter your booking tickets:")
+			fmt.Scanln(&userTickets)
 
-	// booking[0] = fname + " " +lname
-	booking = append(booking,fname + " " +lname )
+			reminingTickets -=  userTickets
 
-	// fmt.Printf("The whole array is :%v\n",booking)
-	// fmt.Printf("The first array is: %v\n",booking[0])
-	// fmt.Printf("The array type is: %T\n",booking[0])
-	// fmt.Printf("The array length is: %d\n",len(booking))
+			// booking[0] = fname + " " +lname
+			booking = append(booking,fname + " " +lname )
 
-	fmt.Printf("The whole slice is :%v\n",booking)
-	fmt.Printf("The first slice is: %v\n",booking[0])
-	fmt.Printf("The slice type is: %T\n",booking[0])
-	fmt.Printf("The slice length is: %d\n",len(booking))
+			// fmt.Printf("The whole array is :%v\n",booking)
+			// fmt.Printf("The first array is: %v\n",booking[0])
+			// fmt.Printf("The array type is: %T\n",booking[0])
+			// fmt.Printf("The array length is: %d\n",len(booking))
 
-	fmt.Printf("Thank you, %s %s. You are %d years old, your email is %s, and you have booked %d tickets.\n",
-    fname, lname, age, email, userTickets)
+			fmt.Printf("The whole slice is :%v\n",booking)
+			fmt.Printf("The first slice is: %v\n",booking[0])
+			fmt.Printf("The slice type is: %T\n",booking[0])
+			fmt.Printf("The slice length is: %d\n",len(booking))
 
-	fmt.Printf("%v tickets remaining for %v\n",reminingTickets, conferenceName)
+			fmt.Printf("Thank you, %s %s. You are %d years old, your email is %s, and you have booked %d tickets.\n",
+			fname, lname, age, email, userTickets)
 
+			fmt.Printf("%v tickets remaining for %v\n",reminingTickets, conferenceName)
 
-	}
+			firstNames := []string{}
+			for _, booking := range booking{
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames,names[0])
+			}
+
+			fmt.Printf("These are all about our booking application:%v",booking)
+		}
+
+}
